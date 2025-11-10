86cbd93fe26c617f682d9742e5623fc167120481
VictorGabrielQS <victor.git24@gmail.com>
2025-11-10 06:24:14 -0300

    star5t controller

---
+++ src/main/java/VictorCode/AuthModule/controller/AuthController.java
@@ -0,0 +1,12 @@
+package VictorCode.AuthModule.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    // Controller - Criar Novo Usuario
+
+
+}
