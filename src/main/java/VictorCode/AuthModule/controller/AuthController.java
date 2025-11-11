be4cb2a049458618715ac1f25508907729e895e6
VictorGabrielQS <victor.git24@gmail.com>
2025-11-11 06:44:11 -0300

    create controller AuthController.java

--- src/main/java/VictorCode/AuthModule/controller/AuthController.java
+++ src/main/java/VictorCode/AuthModule/controller/AuthController.java
@@ -1,12 +1,35 @@
 package VictorCode.AuthModule.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import VictorCode.AuthModule.dto.LoginRequest;
+import VictorCode.AuthModule.dto.RegisterRequest;
+import VictorCode.AuthModule.model.User;
+import VictorCode.AuthModule.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    // Controller - Criar Novo Usuario
+    private  final AuthService authService;
 
+
+    // Controller - Criar Novo Usuario
+    @PostMapping("/registerUser")
+    public ResponseEntity<User> criarUsuario(@RequestBody  RegisterRequest registerRequest){
+        User user = authService.register(registerRequest);
+        return ResponseEntity.ok(user);
+    }
+
+
+    // Controller - Logar Usuario
+    @GetMapping("/loginUser")
+    public ResponseEntity<String> loginUsuario(@RequestBody LoginRequest loginRequest){
+        String token = authService.login(loginRequest);
+        return ResponseEntity.ok(token);
+    }
 
 }
